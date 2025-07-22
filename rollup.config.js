4b65e2e4bbbbc52f6bf0b0bd68f4256d0db232ec
MrKBear <mrkbear@qq.com>
2025-07-22 14:59:57 +0000

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,43 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 
-export default defineConfig({
-    input: "src/index.ts",
-    output: {
-        dir: "dist",
-        format: "esm"
+export default defineConfig([
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                format: "esm",
+                file: "dist/index.esm.js"
+            },
+            {
+                format: "cjs",
+                file: "dist/index.cjs.js"
+            },
+            {
+                format: "umd",
+                name: "EDataView",
+                file: "dist/index.umd.js"
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfig: "./tsconfig.app.json"
+            })
+        ]
     },
-    plugins: [
-        typescript({
-            tsconfig: "./tsconfig.app.json"
-        })
-    ]
-});
+    {
+        input: "src/index.ts",
+        output: {
+            format: "umd",
+            name: "EDataView",
+            file: "dist/index.umd.mini.js"
+        },
+        plugins: [
+            typescript({
+                tsconfig: "./tsconfig.app.json"
+            }),
+            terser()
+        ]
+    }
+]);
